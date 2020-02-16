3f4617701f6cb06d4018851e83a1d7e83f63e835
calebmg <borismoran2000@hotmail.com>
2020-02-16 09:16:05 -0600

    Vistas

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Proyecto_LFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstruction1 = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInstruction1
+            // 
+            this.lblInstruction1.AutoSize = true;
+            this.lblInstruction1.Location = new System.Drawing.Point(61, 52);
+            this.lblInstruction1.Name = "lblInstruction1";
+            this.lblInstruction1.Size = new System.Drawing.Size(183, 13);
+            this.lblInstruction1.TabIndex = 0;
+            this.lblInstruction1.Text = "Ingrese el archivo que desea analizar";
+            this.lblInstruction1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(64, 68);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(180, 20);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(64, 94);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadFile.TabIndex = 2;
+            this.btnLoadFile.Text = "Cargar";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLoadFile);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.lblInstruction1);
+            this.Name = "Form1";
+            this.Text = "Go 0.0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstruction1;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnLoadFile;
+    }
+}
+
